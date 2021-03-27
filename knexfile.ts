c36e1983600af48d7e44818076082168b49fedba
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-27 17:27:07 -0300

    config: knex connection

---
+++ knexfile.ts
@@ -0,0 +1,10 @@
+require('dotenv').config()
+import path from 'path'
+
+module.exports = {
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'migrations')
+  }
+}
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
+require('dotenv').config()
 import knex from 'knex'
 
 export const connection = knex({
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-  searchPath: ['knex', 'public'],
+  connection: process.env.PG_CONNECTION_STRING
 })
