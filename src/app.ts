2a07b98074a8fcd4584c77676a69eefaba9dcd55
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-27 15:27:00 -0300

    code: first route

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
 
+import express, { Express } from 'express'
+
+const app: Express = express()
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
